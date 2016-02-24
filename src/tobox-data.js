999e5bc3faf136bc9b1a5294de7d32517e433456
sergeypopov <sergey.popov.istra@gmail.com>
2016-02-24 11:40:23 +0300

    vendorCode

--- src/tobox-data.js
+++ src/tobox-data.js
@@ -1,8 +1,7 @@
 window.categories = {};
 window.tobox_uuid = {'name': 'Tobox UUID (for update)', 'value': 'id'};
-window.vendor_code = {'name': 'Vendor Code', 'value': 'venorCode'};
 window.tobox_pictures = 'pictures';
-window.products = {'Картинка': 'pictures', 'Заголовок': 'title', 'Описание': 'description', 'Видимость': 'hidden', 'Цена': 'price'};
+window.products = {'Картинка': 'pictures', 'Заголовок': 'title', 'Описание': 'description', 'Видимость': 'hidden', 'Цена': 'price', 'Vendor Code': 'vendorCode'};
 window.required = ['pictures','title','description','price']
 window.messages = {'not_selected': 'Not selected'};
 
